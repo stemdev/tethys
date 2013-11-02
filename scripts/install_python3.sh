b6c89806d5c96c079e84b928a6d8d0a36003a551
steamlight <kelleydv@gmail.com>
2013-11-02 15:49:18 -0700

    python and git installation scripts

---
+++ scripts/install_python3.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# latest from python.org
+wget http://www.python.org/ftp/python/3.3.2/Python-3.3.2.tar.bz2
+
+# unpack tarball
+tar jxvf Python-3.3.2.tar.bz2
+cd Python-3.*
+
+# install
+./configure
+make
+sudo make install
+
+# clean up
+sudo rm -rf Python-3*
+
+
