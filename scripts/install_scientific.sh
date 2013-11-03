b5f1758763a1b456aef5e8645772ad176b3fa503
steamlight <kelleydv@gmail.com>
2013-11-02 17:02:20 -0700

    numpy working?

---
+++ scripts/install_scientific.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# Install numpy, matplotlib
+
+# dependencies
+#ATLAS (BLAS/LAPACK implementation)
+sudo yum -y install atlas-sse3-devel.x86_64
+#sudo yum -y install atlas.x86_64
+
+#Fortran
+sudo yum -y install gcc-gfortran.x86_64
+
+#cython
+wget http://cython.org/release/Cython-0.19.2.tar.gz
+tar zxvf Cython-*
+cd Cython-*
+sudo python3 setup.py install
+rm -rf Cython-*
+
+# latest numpy
+git clone git://github.com/numpy/numpy.git numpy
+cd numpy
+sudo python3 setup.py build --fcompiler=gnu95
+cd ..
+rm -rf numpy
+
