e9a0be50f173e4d46aec79b82119c79b1f05e878
kelleydv <kelleydv@gmail.com>
2013-12-31 11:51:51 -0800

    experimenting

--- html/index.php
+++ html/index.php
@@ -1,8 +1,9 @@
 <!DOCTYPE html>
 <html>
     <head>
-            <meta charset="utf-8">
-            <title>My Login</title>
+        <meta charset="utf-8">
+        <title>My Login</title>
+        <link rel="stylesheet" type="text/css" href="styles/styles.css">
     </head>
     <body>
      
