1b53cbc4d725d21c617db452fd6e1aef9ae89910
kelleydv <kelleydv@gmail.com>
2013-12-31 12:18:37 -0800

    guess the secret password...

--- html/index.php
+++ html/index.php
@@ -1,4 +1,17 @@
-<!DOCTYPE html>
+<?PHP
+if( isset($_POST['username'])){
+    $username = $_POST['username'];
+    $password = $_POST['password'];
+    
+    if( $username=="dv" && $password=="NewEnglandClamChowder"){
+        echo "correct";
+    } else {
+        echo "incorrect";
+    }
+
+} else {
+
+?><!DOCTYPE html>
 <html>
     <head>
         <meta charset="utf-8">
@@ -6,8 +19,13 @@
         <link rel="stylesheet" type="text/css" href="styles/styles.css">
     </head>
     <body>
-     
+        <form method="post" action="<?PHP echo $_SERVER["PHP_SELF"]; ?>">
+            <input type="text" name="username">
+            <input type="password" name="password">
+            <input type="submit" value="Login">
+        </form> 
     </body>
 </html>
-
-
+<?PHP
+}
+?>
