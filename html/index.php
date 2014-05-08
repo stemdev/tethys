522325945b3ddf267ed97a4967a6e4340b867fa9
kelleydv <kelleydv@gmail.com>
2014-05-07 21:04:42 -0700

    almost

--- html/index.php
+++ html/index.php
@@ -6,7 +6,7 @@ if( isset($_POST['username'])){
     if( $username=="dv" && $password=="NewEnglandClamChowder"){
         echo "correct";
     } else {
-        echo "incorrect";
+        echo "almost correct";
     }
 
 } else {
